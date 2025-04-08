ddc28fa7b821e1877378d30264ff161b2d2dcf2c
okg233 <lurkerlair@gmail.com>
2025-04-07 22:20:08 -0400

    Update Netlify and Next.js configurations

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'www.onlysmash.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig 
